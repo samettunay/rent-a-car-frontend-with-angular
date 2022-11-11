7d41685b8f1d4441c8746623fadf900d37fe4e24
samet tunay <samet.tunay12@gmail.com>
2022-11-11 16:21:21 +0300

    Beginning Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { CarComponent } from './components/car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/rental.ts
@@ -0,0 +1,8 @@
+export interface Rental{
+    id:number,
+    brandName:number,
+    FirstName:string,
+    LastName:string,
+    rentDate:Date,
+    returnDate:Date
+}
